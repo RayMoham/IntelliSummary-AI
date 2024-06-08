5515f73fd22e86919067072c7cdf6be76bbc8baa
Rayan Kaleem Mohammed <rayanmd99@gmail.com>
2024-06-08 16:22:47 -0400

    Connect chnages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <main>
       <div className='main'>
+        
         <div className='gradient' />
       </div>
 
--- src/services/store.js
+++ src/services/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { articleApi } from "./article";
 
 export const store = configureStore({
+    
     reducer: {
         [articleApi.reducerPath]: articleApi.reducer,
     },
