16918240c192dca025725cc12de744477ba4b838
Rayan Kaleem Mohammed <rayanmd99@gmail.com>
2024-06-16 16:10:58 -0400

    path note

--- src/services/article.js
+++ src/services/article.js
@@ -7,6 +7,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
+            
             headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
