61179025eae0d94e78b23c6a7dd3b71ce8b46636
Rayan Kaleem Mohammed <rayanmd99@gmail.com>
2024-06-18 23:06:26 -0400

    main altercations

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { store } from "./services/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   
   <React.StrictMode>
+    
     <Provider store={store}>
       <App />
     </Provider>
