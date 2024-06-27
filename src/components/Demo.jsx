70f1857b85080154c98daa4c9c2838acf162845a
Rayan Kaleem Mohammed <rayanmd99@gmail.com>
2024-06-27 12:53:06 -0400

    reading changes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
+
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
